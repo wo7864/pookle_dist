6620a0d2a3ab2ba0f41a6ebc8e0b693cf058555d
wo7864 <wo78644@gmail.com>
2019-01-15 19:00:29 +0900

    deploy

--- dist/uni/polyfills.js
+++ dist/uni/polyfills.js
@@ -10202,7 +10202,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\YJ\pookle\client\UNI\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\YJ\pookle\client\uni\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
